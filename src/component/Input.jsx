d798e8c67e7cdb73e3857ab15c747e29ab556b7b
labalabaya <94170181+DonitoAkbar@users.noreply.github.com>
2022-10-24 02:12:42 +0700

    Komponen Input

---
+++ src/component/Input.jsx
@@ -0,0 +1,7 @@
+export default function Input() {
+    return (
+        <input className=" justify-self-center mx-auto  mt-5 block w-40 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 " type="number" placeholder="NIM Mahasiswa" id="input_text" />
+
+    )
+}
